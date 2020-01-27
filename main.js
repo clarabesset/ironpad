07f10fbe0af2359985fa603ae60f14fba078d473
grillz <grillz@grillzs-MacBook-Pro.local>
2020-01-27 15:16:53 +0100

    wesh

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-import * as Tone from "Tone";
 
-var kick1 = new Tone.Player("https://dl.dropboxusercontent.com/s/q9ffbq1esfufado/kick3.wav?dl=0");
-var kick2 = new Tone.Player("kick2","https://dl.dropboxusercontent.com/s/fx62nliy7zgv0rg/kick1.wav?dl=0");
-const kick3 = new Tone.Player("./sounds/Chants/ChantBreath001.wav").toMaster();
-var clap1 = new Tone.Player("clap1","https://dl.dropboxusercontent.com/s/xlv8x19wvg9ynh8/clap1.wav?dl=0");
-var clap2 = new Tone.Player("clap2","https://dl.dropboxusercontent.com/s/og84e9s5g4z8wyx/clap2.wav?dl=0");
-var clap3 = new Tone.Player("clap3","https://dl.dropboxusercontent.com/s/4741pvdfsv5x82b/clap3.wav?dl=0");
-var bass1 = new Tone.Player("bass1","https://dl.dropboxusercontent.com/s/oc7yrdrk64w66ck/bass1.wav?dl=0");
-var bass2 = new Tone.Player("bass2","https://dl.dropboxusercontent.com/s/73ejwnwpcxlxte9/distor.wav?dl=0");
-var hoover1 = new Tone.Player("hoover1","https://dl.dropboxusercontent.com/s/x5oc2av9xoqfydq/hoover.wav?dl=0");
-var fx = new Tone.Player("fx","https://dl.dropboxusercontent.com/s/nd79xe2s83jg4sh/fxs.wav?dl=0");
+var kick1 = new Tone.Player("./Kick/Kick001.wav").toMaster();
+var kick2 = new Tone.Player("./Kick/Kick002.wav").toMaster();
+var kick3 = new Tone.Player("./Kick/Kick003.wav").toMaster();
+var kick4 = new Tone.Player("./Kick/Kick004.wav").toMaster();
+var kick5 = new Tone.Player("./Kick/Kick005.wav").toMaster();
+var kick6 = new Tone.Player("./Kick/Kick006.wav").toMaster();
+var kick7 = new Tone.Player("./Kick/Kick007.wav").toMaster();
+var kick8 = new Tone.Player("./Kick/Kick008.wav").toMaster();
+var kick9 = new Tone.Player("./Kick/Kick009.wav").toMaster();
 
 document.getElementById("kick1").onclick = () => kick1.start();
 document.getElementById("kick2").onclick = () => kick2.start();
 document.getElementById("kick3").onclick = () => kick3.start();
-document.getElementById("clap1").onclick = () => clap1.play();
-document.getElementById("clap2").onclick = () => clap2.play();
-document.getElementById("clap3").onclick = () => clap3.play();
-document.getElementById("bass1").onclick = () => bass1.play();
-document.getElementById("bass2").onclick = () => bass2.play();
-document.getElementById("hoover1").onclick = () => hoover1.play();
-document.getElementById("fx").onclick = () => fx.play();
+document.getElementById("kick4").onclick = () => kick4.play();
+document.getElementById("kick5").onclick = () => kick5.play();
+document.getElementById("kick6").onclick = () => kick6.play();
+document.getElementById("kick7").onclick = () => kick7.play();
+document.getElementById("kick8").onclick = () => kick8.play();
+document.getElementById("kick9").onclick = () => kick9.play();
 
 
